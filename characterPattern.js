be257b09ee37453f6afb31221715bd01b96e69d1
muhammedadil01 <muhammedadil932@gmail.com>
2023-07-19 13:53:52 +0530

    add more

--- characterPattern.js
+++ characterPattern.js
@@ -9,6 +9,8 @@ for (let i = 1; i <= n; i++) {
 }
 console.log(string);
 
+//---------------------------------------------
+
 var n = 5;
 var string = "";
 
@@ -20,3 +22,44 @@ for (let i = n; i >= 1; i--) {
 }
 
 console.log(string);
+ 
+//-----------------------------------------------------
+
+var size = 5
+
+for (let i = 0; i < size; i++) {
+  let line = '';
+  for (let j = 0; j < size; j++) {
+    const char = String.fromCharCode(65 + i); 
+    line += char + ' ';
+  }
+  console.log(line);
+}
+
+//------------------------------------------------------
+
+var size = 5
+
+for (let i = 0; i < size; i++) {
+  let line = '';
+  for (let j = 0; j < size - i - 1; j++) {
+    line += ' ';
+  }
+  for (let k = 0; k < 2 * i + 1; k++) {
+    const char = String.fromCharCode(65 + i);
+    line += char;
+  }
+  console.log(line);
+}
+
+for (let i = size - 2; i >= 0; i--) {
+  let line = '';
+  for (let j = 0; j < size - i - 1; j++) {
+    line += ' ';
+  }
+  for (let k = 0; k < 2 * i + 1; k++) {
+    const char = String.fromCharCode(65 + i);
+    line += char;
+  }
+  console.log(line);
+}
--- numberPattern.js
+++ numberPattern.js
@@ -10,14 +10,44 @@ for(let i= 1 ; i<=5 ;i++){
 
 console.log(str);
 
-
+//----------------------------------------------------
 
 for (let i = 5; i >= 1; i--) {
-    let row = '';
+    var row = '';
     for (let j = 1; j <= i; j++) {
       row += j + ' ';
     }
     console.log(row);
   }
 
+  //-------------------------------------------------------
 
+
+var rows = 5
+
+   for (let i = 1; i <= rows; i++) {
+    let row = "";
+    for (let j = 1; j <= rows - i; j++) {
+      row += " ";
+    }
+    for (let k = 1; k <= i; k++) {
+      row += k + " ";
+    }
+    console.log(row);
+  }
+
+//--------------------------------------------------------------
+
+
+var rows = 5
+
+for (let i = 1; i <= rows; i++) {
+  var row = "";
+  for (let j = 1; j <= rows - i; j++) {
+    row += "  ";
+  }
+  for (let k = 1; k <= i; k++) {
+    row += k + " ";
+  }
+  console.log(row);
+}
